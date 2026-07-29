dd00eaed6a270dde0071230bae70423ed5c3a53b
Ayush <ayush.d.singh@oracle.com>
2026-07-29 12:46:23 +0530

    Initial Commit

---
+++ src/main/java/com/test/helloworldwebapp/HelloWorldWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.test.helloworldwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldWebAppApplication.class, args);
+    }
+
+}
