e7c7d5927cfab8dd573512988ddb15f2cbe9a143
Ayush <ayush.d.singh@oracle.com>
2026-07-29 13:23:51 +0530

    commit 2

--- src/main/java/com/test/helloworldwebapp/HelloWorldWebAppApplication.java
+++ src/main/java/com/test/helloworldwebapp/HelloWorldWebAppApplication.java
@@ -3,11 +3,10 @@ package com.test.helloworldwebapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.test.helloworldwebapp")
 public class HelloWorldWebAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldWebAppApplication.class, args);
     }
-
 }
