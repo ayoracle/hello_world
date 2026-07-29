2cb8be350488d75910a4a5b30ed55633f0386a03
Ayush <ayush.d.singh@oracle.com>
2026-07-29 13:30:48 +0530

    commit 3

--- src/test/java/com/test/helloworldwebapp/HelloWorldWebAppApplicationTests.java
+++ src/test/java/com/test/helloworldwebapp/HelloWorldWebAppApplicationTests.java
@@ -1,26 +1,15 @@
 package com.test.helloworldwebapp;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
-import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest(classes = HelloWorldWebAppApplication.class)
-@AutoConfigureMockMvc
 class HomeControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+    private final HomeController controller = new HomeController();
 
     @Test
-    void helloEndpointReturnsHelloWorld() throws Exception {
-        mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("Hello World"));
+    void helloReturnsHelloWorld() {
+        assertEquals("Hello World", controller.hello());
     }
 }
